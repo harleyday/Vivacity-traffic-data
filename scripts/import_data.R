4b8de27236b99714a6015fa94cf427c2fb7383fc
harleyday <harley.day4@gmail.com>
2021-12-15 10:11:59 +0000

    Add import of new data from Appledorn.

--- scripts/import_data.R
+++ scripts/import_data.R
@@ -4,13 +4,41 @@ library(lubridate)
 library(here)
 
 ## import the traffic data from the excel spreadsheet, and write a csv version of the cleaned-up version
-traffic <-
-  read_excel(here("data", "raw_data", "Butter Hill Traffic Sensor.xlsx"),
-             sheet = "Butter Hill Traffic Sensor",
-             .name_repair = "universal") %>%
+butter_hill_traffic <-
+  read_excel(
+    here("data",
+         "raw_data",
+         "Butter Hill Traffic Sensor.xlsx"),
+    sheet = "Butter Hill Traffic Sensor",
+    .name_repair = "universal"
+  ) %>%
   rename_with(toupper) %>% # Modify all names to uppercase
   rename(DATE = UTC.TIME) %>% # rename the clunky date column to "DATE" for simplicity
   mutate(DAY = wday(DATE, label = TRUE)) %>% # add a "day of the week" column
   write_csv(here("data", "processed_data", "traffic_data.csv"))
 
-            
+appledorn_traffic <-
+  read_csv(
+    here(
+      "data",
+      "raw_data",
+      "Apeldoorn Drive trafffic sensor data far end.csv"
+    ),
+    name_repair = "universal"
+  ) %>%
+  rename_with(toupper) %>% # Modify all names to uppercase
+  rename(DATE = UTC.TIME) %>% # rename the clunky date column to "DATE" for simplicity
+  mutate(DAY = wday(DATE, label = TRUE))
+
+appledorn_15_minute_22Oct2021_traffic <-
+  read_csv(
+    here(
+      "data",
+      "raw_data",
+      "Apeldoorn Drive far end traffic sensor data 24 October 2021.csv"
+    ),
+    name_repair = "universal"
+  ) %>%
+  rename_with(toupper) %>% # Modify all names to uppercase
+  rename(DATE = UTC.TIME) %>% # rename the clunky date column to "DATE" for simplicity
+  mutate(DAY = wday(DATE, label = TRUE))
