45afa643f51e353c1c4208eb19e9c21c76d2e593
harleyday <harley.day4@gmail.com>
2022-01-01 11:02:49 +0000

    Add Sutton-wide data processing to reproduce John's work in the excel document.

--- scripts/process.R
+++ scripts/process.R
@@ -12,7 +12,7 @@ transport_modes <-
 ## calculates the average number of each model at each location and direction over the entire duration
 average <- traffic %>%
   group_by(COUNTLINENAME, DIRECTION) %>%
-  summarise_at(all_of(transport_modes), list(mean = mean, sd = sd))
+  summarise(across(all_of(transport_modes), list(mean = mean, sd = sd)))
 
 ## compare average numbers for each mode in each direction on weekdays and weekends
 wkday_numbers <- traffic %>%
@@ -34,3 +34,41 @@ total_counts <- traffic %>%
 total_counts_22Oct2021_Appledorn <- appledorn_15_minute_22Oct2021_traffic %>%
   group_by(LOCAL.TIME..SENSOR.) %>%
   summarise(across(all_of(transport_modes), sum))
+
+##### Sutton-wide data
+## compare average numbers for each mode in each direction on weekdays and weekends
+wkday_numbers <- Sutton_wide_traffic_data %>%
+  filter(!DAY %in% c("Sat", "Sun")) %>% # filter for just working week
+  group_by(COUNTLINENAME, DIRECTION) %>%
+  summarise(across(all_of(transport_modes), mean))
+
+wkend_numbers <- Sutton_wide_traffic_data %>%
+  filter(DAY %in% c("Sat", "Sun")) %>% # filter for just weekends
+  group_by(COUNTLINENAME, DIRECTION) %>%
+  summarise(across(all_of(transport_modes), mean))
+
+## calculates the average number of each model at each location and direction over the entire duration
+summary_stats_either_direction <- Sutton_wide_traffic_data %>%
+  group_by(COUNTLINENAME) %>%
+  summarise_at(all_of(transport_modes), list(total = sum, mean = mean, sd = sd))
+
+sum_either_direction_each_day <- Sutton_wide_traffic_data %>%
+  group_by(DATE, COUNTLINENAME) %>%
+  summarise(across(all_of(transport_modes), sum)) %>%
+  group_by(DATE, COUNTLINENAME) %>%
+  mutate(TOTAL = sum(c_across(cols = all_of(transport_modes))))
+
+number_days_with_zero_observations <- sum_either_direction_each_day %>%
+  filter(TOTAL==0) %>%
+  group_by(COUNTLINENAME) %>%
+  summarise(n = n())
+
+## filter out those days with zero observations as they're probably days when the sensor was inactive
+sum_nonzero_either_direction_each_day <- sum_either_direction_each_day %>%
+  filter(TOTAL>0)
+
+## filter summary statistics by date
+summary_stats_26Jul2021_to_19Dec2021 <- sum_nonzero_either_direction_each_day %>%
+  group_by(COUNTLINENAME) %>%
+  filter(DATE <= as_date("2021-12-29") && DATE >= as_date("2021-07-26")) %>%
+  summarise(across(all_of(transport_modes), list(total = sum, mean = mean)))
